5c99248d73290c47bb52b517c9be77f285767030
Deyvi Parra <deyvi.pr@gmail.com>
2022-07-07 11:31:58 -0500

    CHange code

--- app.js
+++ app.js
@@ -1,63 +1,20 @@
-/**
- * Copyright 2016-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the license found in the
- * LICENSE file in the root directory of this source tree.
- */
+var express = require('express')
+  , bodyParser = require('body-parser');
 
- var bodyParser = require('body-parser');
- var express = require('express');
- var app = express();
- var xhub = require('express-x-hub');
- 
- app.set('port', (process.env.PORT || 5000));
- app.listen(app.get('port'), console.log('Server started on port ' + app.get('port')));
- 
- app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
- app.use(bodyParser.json());
- 
- var token = process.env.TOKEN || 'token';
- var received_updates = [];
- 
- app.get('/', function(req, res) {
-   console.log("req", req.body);
-   res.status(200).send('verified');
-//    res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
- });
- 
- app.get(['/facebook', '/instagram'], function(req, res) {
-   if (
-     req.query['hub.mode'] == 'subscribe' &&
-     req.query['hub.verify_token'] == token
-   ) {
-     res.send(req.query['hub.challenge']);
-   } else {
-     res.sendStatus(400);
-   }
- });
- 
- app.post('/facebook', function(req, res) {
-   console.log('Facebook request body:', req.body);
- 
-   if (!req.isXHubValid()) {
-     console.log('Warning - request header X-Hub-Signature not present or invalid');
-     res.sendStatus(401);
-     return;
-   }
- 
-   console.log('request header X-Hub-Signature validated');
-   // Process the Facebook updates here
-   received_updates.unshift(req.body);
-   res.sendStatus(200);
- });
- 
- app.post('/instagram', function(req, res) {
-   console.log('Instagram request body:');
-   console.log(req.body);
-   // Process the Instagram updates here
-   received_updates.unshift(req.body);
-   res.sendStatus(200);
- });
- 
- app.listen();
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
+
+app.get("/", function (request, response) {
+  response.send('Simple WhatsApp Webhook tester</br>There is no front-end, see server.js for implementation!');
+});
+
+app.post("/webhook", function (request, response) {
+  console.log('Incoming webhook: ' + JSON.stringify(request.body));
+  response.sendStatus(200);
+});
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
