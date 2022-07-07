c7d1eaee0ae4a326742b4d6a433cc3ce823ab7fc
Deyvi Parra <deyvi.pr@gmail.com>
2022-07-07 12:07:09 -0500

    Comment axios

--- app.js
+++ app.js
@@ -44,21 +44,21 @@ app.post('/webhook', async (req, res) => {
       let phone_number_id = req.body.entry[0].changes[0].value.metadata.phone_number_id
       let from = req.body.entry[0].changes[0].value.messages[0].from // extract the phone number from the webhook payload
       let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body // extract the message text from the webhook payload
-      try {
-        await axios({
-          method: 'POST', // Required, HTTP method, a string, e.g. POST, GET
-          url: 'https://graph.facebook.com/v12.0/' + phone_number_id + '/messages?access_token=' + token,
-          data: {
-            messaging_product: 'whatsapp',
-            to: from,
-            text: { body: 'Ack: ' + msg_body },
-          },
-          headers: { 'Content-Type': 'application/json' },
-        })
-      } catch (e) {
-        console.log('axios Error', e)
-      }
-    }
+    //   try {
+    //     await axios({
+    //       method: 'POST', // Required, HTTP method, a string, e.g. POST, GET
+    //       url: 'https://graph.facebook.com/v12.0/' + phone_number_id + '/messages?access_token=' + token,
+    //       data: {
+    //         messaging_product: 'whatsapp',
+    //         to: from,
+    //         text: { body: 'Ack: ' + msg_body },
+    //       },
+    //       headers: { 'Content-Type': 'application/json' },
+    //     })
+    //   } catch (e) {
+    //     console.log('axios Error', e.req)
+    //   }
+    // }
     res.sendStatus(200)
   } else {
     // Return a '404 Not Found' if event is not from a WhatsApp API
